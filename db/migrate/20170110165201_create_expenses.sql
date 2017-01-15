19a57b5182d5994d53650f44c5026b60b9a28ee1
gkats <giannis.katsanos@gmail.com>
2017-01-15 15:45:34 +0200

    Updated database migration to create expenses table
    The date column should hold timestamp info. Added an index on tag column.

--- db/migrate/20170110165201_create_expenses.sql
+++ db/migrate/20170110165201_create_expenses.sql
@@ -1,7 +1,9 @@
 CREATE TABLE expenses (
   id SERIAL PRIMARY KEY,
   price_cents INTEGER NOT NULL DEFAULT 0 CHECK (price_cents >= 0),
-  date TIMESTAMP NOT NULL,
+  date TIMESTAMP WITH TIME ZONE NOT NULL,
   tag VARCHAR(50) NOT NULL,
   notes TEXT
 );
+
+CREATE INDEX tag_index ON expenses (tag);
