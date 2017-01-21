f26faab1573034e21247cb3ed43b74b372a4470b
gkats <giannis.katsanos@gmail.com>
2017-01-21 17:42:10 +0200

    #27 Support for JWT tokens
    Added "auth" package. Exposes type TokenService with two methods. CreateToken
    creates a JWT token and ValidateToken ensures a token is valid.

---
+++ src/auth/jwt.go
@@ -0,0 +1,44 @@
+package auth
+
+import (
+	jwt "github.com/dgrijalva/jwt-go"
+	"errors"
+)
+
+// TODO add this to a secrets file. Get it from env in production
+var authSecret = "my-secret-key"
+
+type JwtError struct {
+	Err error
+}
+
+func (e JwtError) Error() string {
+	return e.Err.Error()
+}
+
+type TokenService struct {
+}
+
+func NewTokenService() *TokenService {
+	return &TokenService{}
+}
+
+func (t *TokenService) CreateToken() (string, error) {
+	claims := jwt.MapClaims{}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(authSecret))
+}
+
+func (t *TokenService) ValidateToken(tokenString string) error {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("Invalid JWT signing method")
+		}
+		return []byte(authSecret), nil
+	})
+
+	if err != nil || !token.Valid {
+		return &JwtError{Err: errors.New("Invalid JWT token")}
+	}
+	return nil
+}
