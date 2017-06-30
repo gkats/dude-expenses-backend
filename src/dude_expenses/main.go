3737a75915753684d2d2a56e248b267413e72022
Giannis Katsanos <giannis.katsanos@gmail.com>
2017-06-30 11:11:03 +0300

    Remove test credentials for DB url

--- src/dude_expenses/main.go
+++ src/dude_expenses/main.go
@@ -20,7 +20,7 @@ func main() {
 		port  = flag.Int("port", 8080, "The HTTP port the server will listen to.")
 		dbUrl = flag.String(
 			"db_url",
-			"dude_expenses:dudeExpen$es123@localhost:5432/dude_expenses_development",
+			"",
 			"The Postgresql database URL. Should be in 'user:password@host:port/database' format.",
 		)
 	)
